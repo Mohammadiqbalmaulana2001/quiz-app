85296e05bb67485b7012d136f1823bf05bb00470
MOHAMMAD IQBAL MAULANA <firmanpeman@gmail.com>
2024-02-21 21:13:16 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState} from "react";
+// import axios from "axios";
+import "./index.css";
+import { TriviaData } from "./apis/quiz";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const triviaData = TriviaData
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [activeAnswer, setActiveAnswer] = useState(false);
 
+  const NextQuestion = () => {
+      if (currentQuestion < triviaData.length) {
+        setCurrentQuestion(currentQuestion + 1);
+        // combineAllAnswers(triviaData[currentQuestion], triviaData[currentQuestion].correct_answer);
+      } else {
+        console.log("Success")
+      }
+ }
+
+ function removeCharacters(question) {
+    return question.replace(/(&quot\;)/g, "\"").replace(/(&rsquo\;)/g, "\"").replace(/(&#039\;)/g, "\'").replace(/(&amp\;)/g, "\"");
+ }
+
+ function clickAnswer() {
+    setActiveAnswer(!activeAnswer)
+ }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="bg-zinc-600 min-h-screen text-slate-50 flex justify-center py-16">
+        <div className="flex flex-col space-y-6 w-5/12">
+            <h1 className="text-xl py-4 rounded-xl bg-zinc-900 font-bold text-center">Quiz App</h1>
+            <div className="flex flex-col space-y-3 bg-zinc-900 rounded-xl p-4">
+                <div className="flex flex-col space-y-3">
+                    <h2 className="text-lg font-bold">{removeCharacters(triviaData[currentQuestion].question)}</h2>
+                    <ul className="flex flex-col space-y-3 list-none"> 
+                      {triviaData[currentQuestion].incorrect_answers.map((answer, index) => (
+                          <li onClick={clickAnswer} className={activeAnswer ? 'answer-active' : 'answer'} key={index}>{answer}</li>
+                      ))}
+                      
+                      <li onClick={clickAnswer} className={activeAnswer ? 'answer-active' : 'answer'}>{triviaData[currentQuestion].correct_answer}</li>
+                    </ul>
+                </div>
+                <div className="flex flex-row justify-end pr-4">
+                    <button onClick={NextQuestion} className="py-2 px-6 bg-sky-400 rounded-xl w-fit">Next</button>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
